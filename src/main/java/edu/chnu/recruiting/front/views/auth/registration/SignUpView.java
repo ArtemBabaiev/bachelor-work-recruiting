cfc28141151c786ed23f6e821854b0b7b17e98e8
ArtemBabaiev <artdcxx4@gmail.com>
2024-04-28 19:16:39 +0300

    using formlayout for signup

--- src/main/java/edu/chnu/recruiting/front/views/auth/registration/SignUpView.java
+++ src/main/java/edu/chnu/recruiting/front/views/auth/registration/SignUpView.java
@@ -5,10 +5,14 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.History;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
+import com.vaadin.flow.theme.lumo.LumoUtility.Display;
+import com.vaadin.flow.theme.lumo.LumoUtility.JustifyContent;
 
 import edu.chnu.recruiting.exceptions.AlreadyExistsException;
 import edu.chnu.recruiting.front.views.HomeView;
@@ -16,28 +20,32 @@ import edu.chnu.recruiting.front.views.auth.registration.SignUpForm.CancelEvent;
 import edu.chnu.recruiting.front.views.auth.registration.SignUpForm.SaveEvent;
 import edu.chnu.recruiting.services.UserService;
 
-@Route("sign-up") 
+@Route("sign-up")
 @PageTitle("Sign Up")
 @AnonymousAllowed
-public class SignUpView extends Div{
-	
+public class SignUpView extends VerticalLayout {
+
 	private UserService userService;
-	
+
 	private SignUpForm form;
-	
+
 	public SignUpView(UserService userService) {
 		this.userService = userService;
-		setSizeFull();
 		initComponents();
 		configureComponents();
-		
-		add(form);
+		Div test = new Div(form);
+		test.setWidth("450px");
+		test.addClassNames(Display.FLEX, JustifyContent.CENTER, AlignItems.CENTER);
+		this.setJustifyContentMode(JustifyContentMode.CENTER);
+		this.setAlignItems(Alignment.CENTER);
+		setSizeFull();
+		add(test);
 	}
 
 	private void initComponents() {
 		form = new SignUpForm();
 	}
-	
+
 	private void configureComponents() {
 		form.addSaveListener(e -> handleSaveEvent(e));
 		form.addCancelListener(e -> handleCancelEvent(e));
@@ -58,6 +66,5 @@ public class SignUpView extends Div{
 			notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
 		}
 	}
-
 
 }
